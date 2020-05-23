876debec7b1d5ca1e7baa23d748830ad8044fb4b
Taylor McGee <tjmcgee@live.com>
2020-05-23 16:16:28 -0400

    No more relative links. Started Python integration.

--- csproj/Program.cs
+++ csproj/Program.cs
@@ -1,12 +1,188 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 
+
 namespace RNAseq_Data_Analysis
 {
+    class Program
+    {
+        static void Main(string[] Arguments)
+        {
+            if (Arguments[0] == "f")
+            {
+                datacleaner();
+            }
+            try
+            {
+                Pycontroller();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error in Accessing Python Code. Error {e.Message}");
+            }
+        }
+        static void Pycontroller ()
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = @"C:\Program Files\Python36\python.exe";
+            var script = @"C:\Programs\RNAseqAnalysis\linearregressionmodel.py";
+            var patharg = @"C:\Programs\RNAseqAnalysis\data.csv";
+            psi.Arguments = string.Format("\"{0}\"\"{1}\"", script, patharg);
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedicrectStandardError = true;
+            var errors, result = "";
+            using (var process = Process.Start(psi))
+            {
+                errors = process.StandardError.ReadToEnd();
+                result = process.StandardOutput.ReadToEnd();
+            }
+            Console.WriteLine($"Errors: {errors}, Results: {result}");
+        }
+        static void datacleaner()
+        {
+            File.Delete(@"C:\Programs\RNAseqAnalysis\data.csv");
+            string origin = @"C:\Programs\RNAseqAnalysis\Rawtxt";
+            string[] set = Pathfinder(origin);
+            int q = 1;
+            List<Patient> data = new List<Patient>();
+            foreach (string p in set)
+            {
+                //Console.WriteLine("Q Value: "+q.ToString());
+                q++;
+                string identifier = Getid(p);
+                double survival = idsurvival(identifier);
+                Patient p1 = TxttoPatient(p, survival, identifier);
+                data.Add(p1);
+            }
+            List<string> ARTlist = fullartlist(data);
+            populate(ARTlist, data);
+            Console.WriteLine("Data Cleaning: Success.");
+            //Console.ReadKey();
+
+        }
+        static void populate (List<string> ARTlist, List<Patient> data)
+        {
+            int num = 1;
+            string[] ans = new string[data.Count+1];
+            StringBuilder hsb = new StringBuilder();
+            hsb.Append("UUID,Survival");
+            foreach (string a in ARTlist)
+            {
+                hsb.Append($",{a}");
+            }
+            ans[0] = hsb.ToString();
+            foreach (Patient p in data)
+            {
+                List<GeneInfoModel> pdata = p.GeneInfo;
+                Dictionary<string, string> dicdata = new Dictionary<string, string>();
+                foreach (GeneInfoModel tt in pdata)
+                {
+                    dicdata.Add(tt.GeneName, tt.Value.ToString());
+                }
+                string temp1 = p.UUID + "," + p.LifeExpectancy.ToString();
+                StringBuilder sb = new StringBuilder();
+                sb.Append(temp1);
+                foreach(string aaa in ARTlist)
+                {
+                    try
+                    {
+                        string temp2 = "," + dicdata[aaa];
+                        sb.Append(temp2);
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"Data Missing for UUID: {p.UUID}, Gene: {aaa}");
+                        sb.Append(",0");
+                    }
+                } 
+                ans[num] = sb.ToString();
+                ///Console.WriteLine($"Patient Data for UUID {p.UUID} completed as line {num + 1}, and index {num}.");
+                num++;
+            }
+            File.WriteAllLines(@"C:\Programs\RNAseqAnalysis\data.csv", ans);
+            ///Console.WriteLine(File.Exists(@"C:\Programs\RNAseqAnalysis\data.csv").ToString());
+        }
+        static List<string> fullartlist(List<Patient> data)
+        {
+            List<string> comp = new List<string>();
+            foreach (Patient x in data)
+            {
+                List<GeneInfoModel> z = x.GeneInfo;
+                foreach (GeneInfoModel y in z)
+                {
+                    comp.Add(y.GeneName);
+                }
+            }
+            List<string> susinct = comp.Distinct().ToList();
+            string susinctstring = String.Join(",", susinct.ToArray());
+            ///File.WriteAllLines(@"C:\Programs\RNAseq\AnalysisATR.csv", susinct);
+            return susinct;
+        }
+        static double idsurvival(string UUID)
+        {
+            string[] list = File.ReadAllLines(@"C:\Programs\RNAseqAnalysis\Survival.txt");
+            double q = 0;
+            foreach (string s in list)
+            {
+                string[] r = s.Split(',');
+                //Console.WriteLine(s);
+                if(r[0].ToLower() == UUID)
+                {
+                    q = Convert.ToDouble(r[1]);
+                    //Console.WriteLine(r[1]);
+                }
+            }
+            if(q == 0)
+            {
+                Console.WriteLine($"Error identifying survival for UUID: {UUID}");
+            }
+            return q;
+        }
+        static Patient TxttoPatient (string path, double survival, string UUID)
+        {
+            string[] raw = File.ReadAllLines(path);
+            Patient info = new Patient();
+            info.UUID = UUID;
+            info.LifeExpectancy = survival;
+            foreach (string value in raw)
+            {
+                var temp = value.Split('\t');
+                GeneInfoModel spec = new GeneInfoModel();
+                spec.GeneName = temp[0];
+                try
+                {
+                    spec.Value = Convert.ToDouble(temp[1]);
+                    info.GeneInfo.Add(spec);
+                }
+                catch
+                {
+                    Console.WriteLine($"Error in Integer Conversion TxttoPatient. Gene: {temp[0]}");
+                }
+            }
+            return info;
+        }
+        static string[] Pathfinder(string target)
+        {
+            string[] Pathset = Directory.GetFiles(target);
+            return Pathset;
+        }
+        static string Getid(string path)
+        {
+            string resid = @"C:\Programs\RNAseqAnalysis\Rawtxt\";
+            string result = string.Empty;
+            result = path.Remove(0, resid.Length);
+            string[] resid2 = result.Split(".");
+            string fresult = resid2[0];
+            return fresult.ToLower();
+        }
+    }
     public class Patient
     {
         public string UUID { get; set; }
@@ -65,141 +241,4 @@ namespace RNAseq_Data_Analysis
         }
     }
 
-    class Program1
-    {
-        static void Main(string[] args)
-        {
-            string origin = @"\Rawtxt";
-            string[] set = Pathfinder(origin);
-            int q = 1;
-            List<Patient> data = new List<Patient>();
-            foreach (string p in set)
-            {
-                Console.WriteLine("Q Value: "+q.ToString());
-                q++;
-                string identifier = Getid(p);
-                double survival = idsurvival(identifier);
-                Patient p1 = TxttoPatient(p, survival, identifier);
-                data.Add(p1);
-            }
-            List<string> ARTlist = fullartlist(data);
-            //populate(ARTlist, data);
-            Console.WriteLine("Sucess.");
-            Console.ReadKey();
-        }
-        static void populate (List<string> ARTlist, List<Patient> data)
-        {
-            int num = 1;
-            string[] ans = new string[data.Count+1];
-            StringBuilder hsb = new StringBuilder();
-            hsb.Append("UUID,Survival");
-            foreach (string a in ARTlist)
-            {
-                hsb.Append($",{a}");
-            }
-            ans[0] = hsb.ToString();
-            foreach (Patient p in data)
-            {
-                List<GeneInfoModel> pdata = p.GeneInfo;
-                Dictionary<string, string> dicdata = new Dictionary<string, string>();
-                foreach (GeneInfoModel tt in pdata)
-                {
-                    dicdata.Add(tt.GeneName, tt.Value.ToString());
-                }
-                string temp1 = p.UUID + "," + p.LifeExpectancy.ToString();
-                StringBuilder sb = new StringBuilder();
-                sb.Append(temp1);
-                foreach(string aaa in ARTlist)
-                {
-                    try
-                    {
-                        string temp2 = "," + dicdata[aaa];
-                        sb.Append(temp2);
-                    }
-                    catch
-                    {
-                        Console.WriteLine($"Data Missing for UUID: {p.UUID}, Gene: {aaa}");
-                        sb.Append(",0");
-                    }
-                } 
-                ans[num] = sb.ToString();
-                Console.WriteLine($"Patient Data for UUID {p.UUID} completed as line {num + 1}, and index {num}.");
-                num++;
-            }
-            File.WriteAllLines(@"\data.csv", ans);
-        }
-        static List<string> fullartlist(List<Patient> data)
-        {
-            List<string> comp = new List<string>();
-            foreach (Patient x in data)
-            {
-                List<GeneInfoModel> z = x.GeneInfo;
-                foreach (GeneInfoModel y in z)
-                {
-                    comp.Add(y.GeneName);
-                }
-            }
-            List<string> susinct = comp.Distinct().ToList();
-            string susinctstring = String.Join(",", susinct.ToArray());
-            File.WriteAllLines(@"\ATR.csv", susinct);
-            return susinct;
-        }
-        static double idsurvival(string UUID)
-        {
-            string[] list = File.ReadAllLines(@"\Survival.txt");
-            double q = 0;
-            foreach (string s in list)
-            {
-                string[] r = s.Split(',');
-                //Console.WriteLine(s);
-                if(r[0].ToLower() == UUID)
-                {
-                    q = Convert.ToDouble(r[1]);
-                    //Console.WriteLine(r[1]);
-                }
-            }
-            if(q == 0)
-            {
-                Console.WriteLine($"Error identifying survival for UUID: {UUID}");
-            }
-            return q;
-        }
-        static Patient TxttoPatient (string path, double survival, string UUID)
-        {
-            string[] raw = File.ReadAllLines(path);
-            Patient info = new Patient();
-            info.UUID = UUID;
-            info.LifeExpectancy = survival;
-            foreach (string value in raw)
-            {
-                var temp = value.Split('\t');
-                GeneInfoModel spec = new GeneInfoModel();
-                spec.GeneName = temp[0];
-                try
-                {
-                    spec.Value = Convert.ToDouble(temp[1]);
-                    info.GeneInfo.Add(spec);
-                }
-                catch
-                {
-                    Console.WriteLine($"Error in Integer Conversion TxttoPatient. Gene: {temp[0]}");
-                }
-            }
-            return info;
-        }
-        static string[] Pathfinder(string target)
-        {
-            string[] Pathset = Directory.GetFiles(target);
-            return Pathset;
-        }
-        static string Getid(string path)
-        {
-            string resid = @"C:\DataStorage\Rawtxt\";
-            string result = string.Empty;
-            result = path.Remove(0, resid.Length);
-            string[] resid2 = result.Split(".");
-            string fresult = resid2[0];
-            return fresult.ToLower();
-        }
-    }
 }
--- linearregressionmodel.py
+++ linearregressionmodel.py
@@ -2,12 +2,14 @@ import pandas
 from pandas import DataFrame
 import numpy as np
 from numpy import array
+import sys
 from sklearn import linear_model
 
-data = pandas.read_csv("\data.csv")
+data = pandas.read_csv(str(sys.args[0]))
 print("Data Collection: Success")
 X = data.drop(["UUID", "Survival"], axis = 1)
 Y = data.Survival
 print("Data Construction: Success")
 reg = linear_model.LinearRegression().fit(X, Y)
+print("R score: " +reg.score(X,Y))
 print ("Model Training: Success")
