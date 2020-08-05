1368ee724c2994fd7eaed2b7645f2074466ac977
taytay191 <tjmcgee@live.com>
2020-08-05 09:48:11 -0400

    added commandline functionality

---
+++ RNAseqFinal/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace RNAseqAnalysis_0529
+{
+    class Options
+    {
+        [Option("ModelPath",
+        Default = @"C:\",
+        Required = false,
+        HelpText = "Path to Python Model Script")]
+        public string ModelPath {get; set;}
+
+        [Option("RawDir",
+        Default = @"C:\",
+        Required = false,
+        HelpText = "Path to raw TCGA data")]
+        public string RawDir { get; set; }
+    }
+}
--- RNAseqFinal/Program.cs
+++ RNAseqFinal/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.VisualBasic;
+using CommandLine;
+using RNAseqAnalysis_0529;
 
 namespace RNAseqFinal
 {
@@ -6,11 +9,18 @@ namespace RNAseqFinal
     {
         public class Configurator
         {
-            public string Model { get; set; }
+            public string ModelPath { get; set; }
+            public string RawDir { get; set; }
         }
         static void Main(string[] args)
         {
             Configurator cfg = new Configurator();
+
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
+            {
+                cfg.ModelPath = o.ModelPath;
+                cfg.RawDir = o.RawDir;
+            });
 
         }
     }
