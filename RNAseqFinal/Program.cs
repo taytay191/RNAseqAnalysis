4292e46485eb4c2a6761b273a331d3399f665c7f
taytay191 <tjmcgee@live.com>
2020-08-05 09:22:14 -0400

    initial commit

---
+++ RNAseqFinal/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RNAseqFinal
+{
+    class Program
+    {
+        public class Configurator
+        {
+            public string Model { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Configurator cfg = new Configurator();
+
+        }
+    }
+}
